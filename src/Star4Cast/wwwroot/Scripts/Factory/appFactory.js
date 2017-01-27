52900ade0a9c446dd228c3d006817832d6656a49
work-ranjeet <work.ranjeet@gmail.com>
2017-01-27 09:30:32 +0530

    njnj

---
+++ src/Star4Cast/wwwroot/Scripts/Factory/appFactory.js
@@ -0,0 +1,20 @@
+﻿angular.module('Star4Cast.Factory').factory('AjaxService', function ($http, $q) {
+    return {
+        getPostData: function (url, postdata) {
+            var deferred = $q.defer();
+            $http.post(url, postdata)
+              .then(function (result) {
+                  deferred.resolve(result.data);
+              });
+            return deferred.promise;
+        },
+        getData: function (url, locale) {
+            var deferred = $q.defer();
+            $http.get(url)
+              .then(function (result) {
+                  deferred.resolve(result.data);
+              });
+            return deferred.promise;
+        }
+    }
+});
